809752868a2d440711f627e7b2e43b380293a0bd
Axel8555 <ozaxel85@gmail.com>
2024-02-27 22:59:27 -0600

    Deshacer

--- curriculum-vitae/next.config.mjs
+++ curriculum-vitae/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+<<<<<<< HEAD
 const nextConfig = {output: 'export'};
+=======
+const nextConfig = {};
+>>>>>>> parent of d2428a9 (Build)
 
 export default nextConfig;
