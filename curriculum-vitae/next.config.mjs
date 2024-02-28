b70d85a2085e882f0475eb55507122967290e3b3
Axel8555 <ozaxel85@gmail.com>
2024-02-28 17:25:58 -0600

    Build

--- curriculum-vitae/next.config.mjs
+++ curriculum-vitae/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+<<<<<<< Updated upstream
 const nextConfig = {output:"export"};
 
+=======
+const nextConfig = {
+    basePath: '',
+    assetPrefix: '',
+    output: "export"
+};
+>>>>>>> Stashed changes
 export default nextConfig;
