8529d0baa8f6a1d4ce20f79ea7d83c417a93f4d1
Axel8555 <Axel8555@users.noreply.github.com>
2024-09-25 08:04:00 +0000

    deploy: b007499685a9a5db9db8ddc5bd22ddd49cdcf8e3

---
+++ _next/static/chunks/app/layout-07e5e52cc94df9fe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7007:function(n,e,t){Promise.resolve().then(t.t.bind(t,5204,23)),Promise.resolve().then(t.t.bind(t,9136,23))},9136:function(){},5204:function(){}},function(n){n.O(0,[913,971,23,744],function(){return n(n.s=7007)}),_N_E=n.O()}]);
