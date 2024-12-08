e39b399a302850291dec3604141b9f147560409a
Axel8555 <Axel8555@users.noreply.github.com>
2024-12-08 23:57:54 +0000

    Add SVG support using @svgr/webpack and update TypeScript and Next.js configurations

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,33 @@ const nextConfig = {
       poll: 1000, // Verifica cambios cada segundo
       aggregateTimeout: 300,
     };
+
+    // Configura webpack para manejar archivos SVG, @svgr/webpack
+
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
     return config;
   },
 };
